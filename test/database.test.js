36e2506a33d9a16fee3ad902731d7a149a894166
cmd-kvn <kvn.m.wong@gmail.com>
2017-01-26 17:58:38 -0800

    added rimraf and simpleid to dependencies in package.json created make directory function in database.js created test for database creation

--- test/database.test.js
+++ test/database.test.js
@@ -1,19 +1,41 @@
-const database = require('../lib/database');
 const assert = require('assert');
 const fs = require('fs');
-const dbCreate = database.create('./test-dir');
+const rimraf = require('rimraf');
+const simpleid = require('simpleid');
 
-describe('database.getAll', function(){
-    it('returns an array of all objects', done => {
-        dbCreate.getAll('./test/test-dir', (err, contents) => {
-            if (err) return done(err);
+const database = require('../lib/database');
+const testDir = './test/db-test-dir';
+
+// describe('dbCreate.getAll', function(){
+//     it('returns an array of all objects', done => {
+//         dbCreate.getAll('./test/test-dir', (err, contents) => {
+//             if (err) return done(err);
             
-            assert.deepEqual(
-                contents, 
-                ['BAR', 'FOO', 'QUX']
-            );
+//             assert.deepEqual(
+//                 contents, 
+//                 ['BAR', 'FOO', 'QUX']
+//             );
 
-            done();
-        });
+//             done();
+//         });
+//     });
+// });
+before(done => {
+    database.create(testDir);
+    fs.stat(testDir, (err, stats) => {
+        assert.equal(null, err);
+        done();
+    });
+});
+
+after(done => {
+    rimraf(testDir, [], (err) => {
+        console.log(err);
+        done();
+    });
+});
+
+describe('database.get', () => {
+    it('returns null when the _id can not be found', () => {
     });
 });
