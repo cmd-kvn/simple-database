6a84a39dafe35a0f18112a473cdded0395ec69dd
cmd-kvn <kvn.m.wong@gmail.com>
2017-01-30 10:56:15 -0800

    added mkdirp to dependencies finished tests for get and save finished methods for get and save

--- test/database.test.js
+++ test/database.test.js
@@ -5,6 +5,62 @@ const simpleid = require('simpleid');
 
 const database = require('../lib/database');
 const testDir = './test/db-test-dir';
+const stdout = process.stdout;
+
+var db;
+var testObject = {
+    message: 'I am inside the test object'
+}
+var testObject1 = {
+    message: 'I am inside the test object1'
+}
+
+before(done => {
+    db = database.create(testDir);
+    fs.stat(testDir, (err, stats) => {
+        assert.equal(null, err);
+        done();
+    });
+});
+
+after(done => {
+    rimraf(testDir, [], (err) => {
+        done();
+    });
+});
+
+describe('database.save', () => {
+    it('gives the objectToSave an _id property', (done) =>{
+        db.save('findMe', testObject, (err, result) => {
+            assert.ok(result.hasOwnProperty('_id'));
+            done();
+        })
+    });
+
+});
+
+describe('database.get', () => {
+    it('returns null when the _id can not be found', (done) => {
+        db.get('findMe', 'noooopppeeee', (err, result) => {
+            assert.equal(err, null);
+            done();
+        })
+    });
+
+    it('starts with a saved object', (done) => {
+         db.save('getTest', testObject1, (err, result) => {
+            assert.ok(result.hasOwnProperty('_id'));
+            done();
+        })
+    });
+
+    it('returns the saved object with that id', (done) => {
+            db.get('getTest', testObject1._id, (err, result) => {
+            assert.equal(result._id, testObject1._id);
+            done();
+        });
+    });
+});
 
 // describe('dbCreate.getAll', function(){
 //     it('returns an array of all objects', done => {
@@ -20,22 +76,3 @@ const testDir = './test/db-test-dir';
 //         });
 //     });
 // });
-before(done => {
-    database.create(testDir);
-    fs.stat(testDir, (err, stats) => {
-        assert.equal(null, err);
-        done();
-    });
-});
-
-after(done => {
-    rimraf(testDir, [], (err) => {
-        console.log(err);
-        done();
-    });
-});
-
-describe('database.get', () => {
-    it('returns null when the _id can not be found', () => {
-    });
-});
